b111bca38fba3809618d7a1d2e72bde28c5748d5
ZeeCleaner <109098000+ZeeCleaner@users.noreply.github.com>
2023-05-03 08:31:46 +0200

    The program files

---
+++ script.js
@@ -0,0 +1,27 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var rbutton = document.getElementById("rbutton");
+
+
+
+body.style.background = "linear-gradient(to right, " + color1.value + " , " + color2.value + ")";
+css.textContent = body.style.background + ";" ;
+
+function setlinearGradient() {
+	body.style.background = "linear-gradient(to right, " + color1.value + " , " + color2.value + ")";
+	css.textContent = body.style.background + ";" ;
+}
+
+
+
+color1.addEventListener("input", setlinearGradient);
+
+color2.addEventListener("input", setlinearGradient);
+
+rbutton.addEventListener("click", function() {
+	color1.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	setlinearGradient();
+})
